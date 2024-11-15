5f181187d26b94b2cb49d58840a62b5d4507e3d0
Ozh-Inc <kayex@mail.ru>
2024-11-15 18:52:09 +0300

    Update module_10_1.py

--- module_10_1.py
+++ module_10_1.py
@@ -1,25 +1,37 @@
 import threading
-from time import sleep
+from time import sleep, time
 
 def write_words(word_count, file_name):
     with open(file_name, 'w', encoding='utf-8') as f:
         for w in range(1, word_count + 1):
             f.write(f'Какое-то слово № {w}\n')
-            sleep(0.1)
+            sleep(0.01)
     print(f'Завершилась запись в файл {file_name}')
 
+
+start_time1 = time()
 
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
 
+print(f'Time elapsed {time() - start_time1}s')
 
 thread1 = threading.Thread(target=write_words, args=(10, 'example5.txt'))
 thread2 = threading.Thread(target=write_words, args=(30, 'example6.txt'))
 thread3 = threading.Thread(target=write_words, args=(200, 'example7.txt'))
 thread4 = threading.Thread(target=write_words, args=(100, 'example8.txt'))
+
+start_time2 = time()
+
 thread1.start()
+thread1.join()
 thread2.start()
+thread2.join()
 thread3.start()
+thread3.join()
 thread4.start()
+thread4.join()
+
+print(f'Time elapsed {time() - start_time2}s')
